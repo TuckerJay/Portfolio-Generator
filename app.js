ae334a2a52dd2e69dba728afbfaa69070c1896ce
TuckerJD <tucker.j.dalrymple@gmail.com>
2022-09-03 16:06:40 -0500

    generate projectArr HTML

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 
-//const fs = require('fs');
+const fs = require('fs');
 
-//const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 
 const promtUser = () => {
@@ -166,23 +166,26 @@ const promptProject = portfolioData => {
 
 };
 
+
 promtUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+
+        const pageHTML = generatePage(portfolioData);
+
+
+        fs.writeFile('./index.html', pageHTML, err => {
+
+        if (err) throw new Error(err);
+
+
+        console.log('Page created! Check out index.html in this directory to see it');
+
+        });
+        
     });
 
 
-//const pageHTML = generatePage(name, github);
 
-
-//fs.writeFile('./index.html', pageHTML, err => {
-
-    //if (err) throw err;
-
-
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-
-//});
 
 
